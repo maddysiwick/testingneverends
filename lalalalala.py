3dba26ae3b5bf5801694470d64a2da05fc501bcf
maddysiwick <maddy.siwick@icloud.com>
2024-11-13 18:04:18 -0500

    funky colour time

--- lalalalala.py
+++ lalalalala.py
@@ -1,1 +1,10 @@
 print('what if i like actually jumped out a window')
+def myFirstColour():
+    colour='blue'
+    return colour
+
+def mySecondColour():
+    colour='red'
+    return colour
+
+print(myFirstColour(),' ',mySecondColour())
