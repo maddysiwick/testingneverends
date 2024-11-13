3e38f28c4a68e46f625fd7d01a4c7537a06ad501
maddysiwick <maddy.siwick@icloud.com>
2024-11-13 18:08:23 -0500

    funky words

--- lalalalala.py
+++ lalalalala.py
@@ -1,1 +1,10 @@
 print('what if i like actually jumped out a window')
+def myFirstWord():
+    word='sky'
+    return word
+
+def mySecondWord():
+    word='dirt'
+    return word
+
+print(myFirstWord(),' ',mySecondWord())
